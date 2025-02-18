c07895e2a491ce83dfa4304b3198b461c15c6cd7
WeiXuanYou <62496238+WeiXuanYou@users.noreply.github.com>
2025-02-19 01:35:15 +0800

    modify

--- leetCode150/ArrayString/MergeSortedArray.c
+++ leetCode150/ArrayString/MergeSortedArray.c
@@ -1,11 +1,6 @@
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     if(n == 0) return;
-    else if (m == 0 ){
-        while(nums2Size)  nums1[--nums2Size] = nums2[--n];
-        return;
-    }
-
     while(--nums1Size >= 0){
       if(n > 0  && m > 0){
         if(nums1[m - 1] >= nums2[n - 1]) nums1[nums1Size] = nums1[--m];
