16dc120902e1b1a2ea3b172d1a6370ea2fc6b08a
WeiXuanYou <62496238+WeiXuanYou@users.noreply.github.com>
2025-02-19 01:31:56 +0800

    add merge content

--- leetCode150/ArrayString/MergeSortedArray.c
+++ leetCode150/ArrayString/MergeSortedArray.c
@@ -1,13 +1,17 @@
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    if(nums2Size == 0) return nums1;
-    else if (m == 0 ) return nums2;
-    
-    while(nums1Size-- != 0){
-        if(nums1[m - 1] > nums2[n - 1])
-           nums1[nums1Size] = nums1[--m];
-        else
-            nums1[nums1Size] = nums2[--n];
+    if(n == 0) return;
+    else if (m == 0 ){
+        while(nums2Size)  nums1[--nums2Size] = nums2[--n];
+        return;
     }
-    return nums1;
+
+    while(--nums1Size >= 0){
+      if(n > 0  && m > 0){
+        if(nums1[m - 1] >= nums2[n - 1]) nums1[nums1Size] = nums1[--m];
+        else nums1[nums1Size] = nums2[--n];
+      }
+      else
+        nums1[nums1Size] = (m == 0) ? nums2[--n] : nums1[--m];
+    }
 }
